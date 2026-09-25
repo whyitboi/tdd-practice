e17a3aae485a4ea7bf5fdf38927e643ee72fee88
whyitboi <20414206+whyitboi@users.noreply.github.com>
2026-09-25 09:10:38 +0300

    Adds function to get starting position in the alphabet

--- src/cesarCipher.js
+++ src/cesarCipher.js
@@ -1,13 +1,16 @@
 export function cesarCipher(someString, shiftFactor) {
   const alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
   let result;
+
+  return result;
+}
+
+function getStartPosition(someString, alphabet) {
   for (let i = 0; i <= someString.length; i++) {
-    alphabet.findIndex((letter) => {
+    alphabet.find((letter) => {
       if (letter === someString[0]) {
-        let currenPos = someString[0];
+        return alphabet.findIndexOf(letter);
       }
     });
   }
-
-  return result;
 }
