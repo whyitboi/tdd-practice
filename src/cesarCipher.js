132e0616e7d23c598f6d6c91780dbf1e07b361d0
whyitboi <20414206+whyitboi@users.noreply.github.com>
2026-09-25 09:36:38 +0300

    Adds checkAndWrap to check if the end of the alphabet has been reached and wrap arround to a

--- src/cesarCipher.js
+++ src/cesarCipher.js
@@ -1,15 +1,24 @@
 const alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
 export function cesarCipher(someString, shiftFactor) {
   let result;
-  const startPos = getStartPosition(someString, alphabet);
+  const startPos = getStartPos(someString, alphabet);
   const newPos = getNewPos(startPos, shiftFactor);
   return result;
 }
 
-function getStartPosition(someString, alphabet) {
+function getStartPos(someString, alphabet) {
   return alphabet.indexOf(someString[0]);
 }
 
 function getNewPos(startPos, shiftFactor) {
+  checkAndWrap(startPos + shiftFactor);
   return alphabet.indexOf(alphabet[startPos + shiftFactor]);
+
+  function checkAndWrap(newPos) {
+    if (newPos > 25) {
+      const newShiftFactor = newPos % 25;
+      startPos = 0;
+      shiftFactor = newShiftFactor;
+    }
+  }
 }
