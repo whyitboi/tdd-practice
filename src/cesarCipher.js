c6ef9890fa6007f07417ccb7ade200a842aab754
whyitboi <20414206+whyitboi@users.noreply.github.com>
2026-09-25 10:18:39 +0300

    Adds processString to make a newString without whitespaces and punctuations

--- src/cesarCipher.js
+++ src/cesarCipher.js
@@ -1,13 +1,38 @@
 const alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
 export function cesarCipher(someString, shiftFactor) {
-  let result;
-  const startPos = getStartPos(someString, alphabet);
-  const newPos = getNewPos(startPos, shiftFactor);
+  let result = "";
+  const capitalPos = [];
+  const punctPos = [];
+
+  checkCapPos(someString);
+
+  for (const letter of someString) {
+    let startPos = getStartPos(someString, alphabet);
+    let newPos = getNewPos(startPos, shiftFactor);
+    result = result + alphabet[newPos];
+  }
+  if (capitalPos.length > 0) {
+    result.map((letter) => {
+      capitalPos.forEach((index) => {
+        if (result.indexOf(letter) === index) {
+          result[result.indexOf(letter)] = letter.toUpperCase();
+        }
+      });
+    });
+  }
+
   return result;
 }
 
 function getStartPos(someString, alphabet) {
   return alphabet.indexOf(someString[0]);
+}
+function checkCapPos(someString) {
+  for (const letter of someString) {
+    if (letter.toUpperCas()) {
+      capitalPos.push(someString.indexOf(letter));
+    }
+  }
 }
 
 function getNewPos(startPos, shiftFactor) {
@@ -18,8 +43,21 @@ function getNewPos(startPos, shiftFactor) {
     if (newPos > 25) {
       startPos = 0;
     } else if (newPos < 0) {
-      startPos = 25;
+      //startPos has to be +1 in negative wrapping
+      startPos = 26;
     }
     shiftFactor = newPos % 26;
   }
+}
+function processString(someString) {
+  let tempString = "";
+  someString = someString.toLowerCase();
+  for (const character of someString) {
+    if (alphabet.include(character)) {
+      tempString = tempString + character;
+    } else {
+      punctPos.push[someString.indexOf(character)];
+    }
+  }
+  someString = tempString;
 }
